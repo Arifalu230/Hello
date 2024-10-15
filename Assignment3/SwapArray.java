c8bdc55cf9534da679c5742758e7181af5a3c01c
Arifalu230 <arifalu230@gmail.com>
2024-10-15 20:04:47 +0530

    Assignment 3 Start

---
+++ Assignment3/SwapArray.java
@@ -0,0 +1,46 @@
+
+import java.util.Scanner;
+
+public class SwapArray {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = addArray(n, sc);
+        // printarr(arr);
+        inverse(arr);
+        printarr(arr);
+    }
+
+    public static int[] addArray(int n, Scanner sc) {
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        return arr;
+    }
+
+    public static void printarr(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println("");
+    }
+
+    public static void inverse(int[] arr) {
+        int i = 0;
+        int j = arr.length - 1;
+        int tmp = 0;
+        while (i < j) {
+            if (arr[i] == 0) {
+                i++;
+            } else {
+                tmp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = tmp;
+                i++;
+                j--;
+            }
+        }
+    }
+}
